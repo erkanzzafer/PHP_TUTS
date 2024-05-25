8d65c70d0dd425dcbfdf11b050e93dbbc74239a6
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 13:57:17 +0300

    while usage

---
+++ 8.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+ <?php 
+
+$number=1;
+
+while ($number<= 10) {
+    echo $number."<br>";
+    $number++;
+}
+
+
+?>
+    
+</body>
+</html>
