db445f39490acdf5f3e48bdf76963ebb7c9f38f4
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 14:00:27 +0300

    foreach usage

---
+++ 10.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+ <?php 
+
+$numbers=array(10,20,30,40,50);
+
+foreach($numbers as $number) {
+    echo "".$number."<br>";
+}
+
+
+?>
+    
+</body>
+</html>
