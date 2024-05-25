a8e937f9a59fda5ef68fe28834f0f860a89b3cab
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 19:25:52 +0300

    read cookies

---
+++ 27.php
@@ -0,0 +1,23 @@
+<?php
+
+
+if (isset($_COOKIE['SomeName'])) {
+    echo  $_COOKIE['SomeName'];
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
