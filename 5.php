9fe92ee119f1cd66a7751756c5e3c837f01dbebb
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 13:48:12 +0300

    if else

---
+++ 5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php
+
+if (3<10){
+
+    echo "3 küçüktür 10dan";
+}else if (23>10){
+    echo "23 büyüktür 10dan";
+}
+
+?>
+    
+</body>
+</html>
