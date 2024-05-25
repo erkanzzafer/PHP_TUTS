b3d690b27f5fe42a482cd8b05f6fec66bb1a5432
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 13:36:21 +0300

    array as key value

---
+++ 4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php
+$number =array(10,20,30);
+
+print_r($number);
+
+$names=array("first_name"=> 'Zafer');
+print_r ($names);
+echo $names["first_name"];
+
+?>
+    
+</body>
+</html>
