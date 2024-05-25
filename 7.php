12e2d365dc48e21fc73208937b4897c85f01f193
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 13:55:04 +0300

    switch usage

---
+++ 7.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+ <?php 
+
+$number=2;
+
+switch ($number) {  
+    case 1: 
+        echo $number= 1*10;
+    break;  
+    case 2: echo $number= 2*10;     
+    break;
+}
+
+
+?>
+    
+</body>
+</html>
