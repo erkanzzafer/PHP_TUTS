d8bd01f3f2ff5502fbd3c8978cb89b6651e20626
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 19:13:07 +0300

    set cookies

---
+++ 26.php
@@ -0,0 +1,22 @@
+<?php
+$name = "SomeName";
+$value = 100;
+$expiration = time() + (60 * 60 * 24 * 7);
+setcookie("$name", $value, $expiration);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
