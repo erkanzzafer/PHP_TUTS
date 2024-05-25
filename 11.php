17154dc4253773881484f6e57554981c1a3dcc58
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 14:04:16 +0300

    function usage

---
+++ 11.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+ <?php 
+
+function calculate(){
+    echo "24+52=76";
+}
+
+calculate();
+
+?>
+    
+</body>
+</html>
