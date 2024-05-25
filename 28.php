44c441457478dede0f4b23a4ecabbf1781d49dcc
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 19:28:37 +0300

    set session

---
+++ 28.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+$_SESSION['greeting'] = "Hello Student";
+
+echo $_SESSION['greeting'];
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
