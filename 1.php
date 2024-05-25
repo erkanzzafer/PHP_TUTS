4d0c8864ae8c6c6bb1a5520beb2ecddb32fb4ed3
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 13:19:06 +0300

    variables

---
+++ 1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+ 
+ $name = "zafer";
+ $number=123;
+ $Number=1235;   
+ $NUMBER = 321;
+ echo $Number;
+
+?>
+
+    
+</body>
+</html>
