9c8aa5765029ab5223e1784893df2ebdd883dcb2
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 13:30:24 +0300

    list array

---
+++ 3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php
+
+$numberList=array();
+
+$numberList = array(2,23,234,2345,"string" );
+//print_r ($numberList);
+echo "<br>";
+echo $numberList[0];
+?>
+    
+</body>
+</html>
