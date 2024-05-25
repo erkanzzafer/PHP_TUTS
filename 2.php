59be8d25c188115b21c44830c0e0939b699c2872
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 13:23:04 +0300

    2.php

---
+++ 2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php
+
+echo 56+45;
+echo "<br>";
+echo "a". 4;
+$number1=32;
+$number2= 23;
+echo $number1  + $number2;
+
+
+?>
+    
+</body>
+</html>
