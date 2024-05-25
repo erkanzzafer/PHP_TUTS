889c53f7e346aefd318d8a0783cf1dd8b3073712
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2024-05-25 13:52:26 +0300

    match usage

---
+++ 6.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+ <?php
+$food = 'cake';
+
+ echo $return_value = match ($food) {
+    'apple' => 'This food is an apple',
+    'bar' => 'This food is a bar',
+    'cake' => 'This food is a cake',
+};
+
+?>
+    
+</body>
+</html>
